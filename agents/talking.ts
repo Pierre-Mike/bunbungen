34efe235432903a39afecf958326aa29c17ca83e
pierre-mikelemelinerwood <pm.lemeliner@gmail.com>
2024-06-13 16:29:34 +0200

    add audio

--- agents/talking.ts
+++ agents/talking.ts
@@ -1,15 +1,16 @@
-import type {AssistantCreateParams} from "openai/resources/beta/assistants";
-const openai = new OpenAI()
-import {createAndRunAssistantStream} from "../utils/conversation.ts";
+import type { AssistantCreateParams } from "openai/resources/beta/assistants";
+const openai = new OpenAI();
+import { createAndRunAssistantStream } from "../utils/conversation.ts";
 import OpenAI from "openai";
 
 export const assistantParams: AssistantCreateParams = {
-    name: 'talking-agent',
-    model: 'gpt-4o',
-    instructions: 'you are here to help the user be smarter over time, answer question and bringing the conversation to an hight level. keep in mind english isn\' the first language of the user you have communicate to the user for feedback and clarification after every major step to ensure alignment.',
-    description: 'Talking about anything.',
-}
+  name: "talking-agent",
+  model: "gpt-4o",
+  instructions:
+    "you are here to help the user be smarter over time, answer question and bringing the conversation to an hight level. keep you answer short and concise, keep in mind english isn' the first language of the user you have communicate to the user for feedback and clarification after every major step to ensure alignment.",
+  description: "Talking about anything.",
+};
 
 const assistant = await openai.beta.assistants.create(assistantParams as any);
 
-await createAndRunAssistantStream({assistantId: assistant.id, userMessage: 'use the calculator', audio: true});
+await createAndRunAssistantStream({ assistantId: assistant.id, audio: true });
--- utils/conversation.ts
+++ utils/conversation.ts
@@ -1,140 +1,168 @@
-import type {AssistantStream} from "openai/lib/AssistantStream";
-import {waitUntil} from "./utils.ts";
-import type {Message} from "openai/resources/beta/threads/messages";
+import type { AssistantStream } from "openai/lib/AssistantStream";
+import { waitUntil } from "./utils.ts";
+import type { Message } from "openai/resources/beta/threads/messages";
 import prompts from "prompts";
 import OpenAI from "openai";
-import {mapTools} from "./mapTools.ts";
+import { mapTools } from "./mapTools.ts";
 import fs from "fs";
 import player from "play-sound";
 
-const openai = new OpenAI()
+const openai = new OpenAI();
 
-export type AssistantParams = { assistantId: string, userMessage: string, threadId?: string, audio?: boolean }
+export type AssistantParams = {
+  assistantId: string;
+  userMessage?: string;
+  threadId?: string;
+  audio?: boolean;
+};
 
 export async function createAndRunAssistantStream({
-                                                      assistantId, userMessage, threadId, audio = false
-                                                  }: AssistantParams): Promise<void> {
-    let assistantStream: AssistantStream | undefined = undefined
+  assistantId,
+  userMessage,
+  threadId,
+  audio = false,
+}: AssistantParams): Promise<void> {
+  let assistantStream: AssistantStream | undefined = undefined;
 
-    if (threadId) {
-        // add message from user to thread
-        await openai.beta.threads.messages.create(threadId, {'role': 'user', content: userMessage})
-        assistantStream = openai.beta.threads.runs.stream(threadId, {
-            stream: true,
-            assistant_id: assistantId
-        })
-    } else {
-        assistantStream = openai.beta.threads.createAndRunStream({
-            assistant_id: assistantId,
-            thread: {messages: [{'role': 'user', content: userMessage}]},
-            stream: true,
-        });
-    }
+  if (threadId) {
+    // add message from user to thread
+    await openai.beta.threads.messages.create(threadId, {
+      role: "user",
+      content: userMessage || "continue",
+    });
+    assistantStream = openai.beta.threads.runs.stream(threadId, {
+      stream: true,
+      assistant_id: assistantId,
+    });
+  } else {
+    assistantStream = openai.beta.threads.createAndRunStream({
+      assistant_id: assistantId,
+      thread: { messages: [{ role: "user", content: userMessage || "hello" }] },
+      stream: true,
+    });
+  }
 
-    assistantStream
-        .on('toolCallDone', () => handleToolCallDone(assistantStream))
-        .on('event', handleEvent)
-        .on('end', async () => await handleEnd(assistantStream, audio));
+  assistantStream
+    .on("toolCallDone", () => handleToolCallDone(assistantStream))
+    .on("event", handleEvent)
+    .on("end", async () => await handleEnd(assistantStream, audio));
 }
 
-async function handleToolCallDone(assistantStream: AssistantStream): Promise<void> {
-    console.log('toolCallDone');
-    let run = await waitUntil(['requires_action'], assistantStream.currentRun());
+async function handleToolCallDone(
+  assistantStream: AssistantStream
+): Promise<void> {
+  console.log("toolCallDone");
+  let run = await waitUntil(["requires_action"], assistantStream.currentRun());
 
-    const functionCalled = run?.required_action?.submit_tool_outputs.tool_calls
-        .filter(e => e.type === 'function')
-        .map(e => ({name: e.function.name, arguments: e.function.arguments, toolId: e.id}));
-
-
-    if (!functionCalled) {
-        console.error('#ERROR_MISSING_FUNCTION_CALL : ', functionCalled);
-        console.error('#ERROR_MISSING_FUNCTION_CALL + : ', run);
-        return;
-    }
-
-
-    const allResult = await Promise.all(functionCalled.map(async (e) => {
-        console.log(functionCalled)
-
-        const functionDefinition = mapTools.get(e.name);
-        let output;
-        if (!functionDefinition) {
-            console.error('Function not found : ', e.name)
-            output = `Function "${e.name}" not found. Try again.`;
-        } else {
-            console.log('calling function : ', functionDefinition.function?.function.name, ' with params : ', e.arguments);
-            // vaidatate params
-            const params = functionDefinition.function.parse(e.arguments);
-            try {
-                // @ts-ignore
-                output = JSON.stringify(await functionDefinition.function.function(params))
-
-            } catch (err: any) {
-                console.error(err);
-                output = `Error calling function "${e.name}": ${err.message}`;
-            }
-        }
-        return {
-            output,
-            tool_call_id: e.toolId
-        };
+  const functionCalled = run?.required_action?.submit_tool_outputs.tool_calls
+    .filter((e) => e.type === "function")
+    .map((e) => ({
+      name: e.function.name,
+      arguments: e.function.arguments,
+      toolId: e.id,
     }));
-    await openai.beta.threads.runs.submitToolOutputsAndPoll(run.thread_id, run.id, {
-        tool_outputs: allResult
-    });
+
+  if (!functionCalled) {
+    console.error("#ERROR_MISSING_FUNCTION_CALL : ", functionCalled);
+    console.error("#ERROR_MISSING_FUNCTION_CALL + : ", run);
+    return;
+  }
+
+  const allResult = await Promise.all(
+    functionCalled.map(async (e) => {
+      console.log(functionCalled);
+
+      const functionDefinition = mapTools.get(e.name);
+      let output;
+      if (!functionDefinition) {
+        console.error("Function not found : ", e.name);
+        output = `Function "${e.name}" not found. Try again.`;
+      } else {
+        console.log(
+          "calling function : ",
+          functionDefinition.function?.function.name,
+          " with params : ",
+          e.arguments
+        );
+        // vaidatate params
+        const params = functionDefinition.function.parse(e.arguments);
+        try {
+          // @ts-ignore
+          output = JSON.stringify(
+            await functionDefinition.function.function(params)
+          );
+        } catch (err: any) {
+          console.error(err);
+          output = `Error calling function "${e.name}": ${err.message}`;
+        }
+      }
+      return {
+        output,
+        tool_call_id: e.toolId,
+      };
+    })
+  );
+  await openai.beta.threads.runs.submitToolOutputsAndPoll(
+    run.thread_id,
+    run.id,
+    {
+      tool_outputs: allResult,
+    }
+  );
 }
 
-function handleEvent({event, data}: { event: any; data: any }): void {
-    console.log('event:', JSON.stringify(event));
+function handleEvent({ event, data }: { event: any; data: any }): void {
+  console.log("event:", JSON.stringify(event));
 }
 
+async function handleEnd(
+  assistantStream: AssistantStream,
+  audio: boolean
+): Promise<void> {
+  console.log("end");
+  let run = await waitUntil(["completed"], assistantStream.currentRun());
+  const messages: { data: Message[] } = await openai.beta.threads.messages.list(
+    run.thread_id
+  );
+  const lastMessages = messages.data[0];
+  const lastMessage = lastMessages.content
+    .map((e) => {
+      if (e.type === "text") return e.text.value;
+    })
+    .join(" ");
 
-async function handleEnd(assistantStream: AssistantStream, audio: boolean): Promise<void> {
-    console.log('end');
-    let run = await waitUntil(['completed'], assistantStream.currentRun());
-    const messages: { data: Message[] } = await openai.beta.threads.messages.list(run.thread_id);
-    const lastMessages = messages.data[0];
-    const lastMessage = lastMessages.content.map(e => {
-        if (e.type === 'text') return e.text.value;
-    }).join(' ')
-
-
-    if (audio) {
-        const speech = await openai.audio.speech.create({
-            voice: 'echo',
-            model: 'tts-1',
-            input: lastMessage
-        });
-        const buffer = await speech.arrayBuffer();
-        const audioPlayer = player();
-        const audioFilePath = '.response.mp3';
-        await fs.promises.writeFile(audioFilePath, Buffer.from(buffer));
-        audioPlayer.play(audioFilePath, (err: any) => {
-            if (err) console.error('Error playing audio:', err);
-        });
-    }
-
-
-    console.log('lastMessage:', lastMessage);
-
-
-    let userMessage = '';
-
-
-    const response: { userMessage: string } = await prompts({
-        type: 'text',
-        name: 'userMessage',
-        message: 'Enter your response:',
+  if (audio) {
+    const speech = await openai.audio.speech.create({
+      voice: "echo",
+      model: "tts-1",
+      input: lastMessage,
     });
-    userMessage = response.userMessage;
+    const buffer = await speech.arrayBuffer();
+    const audioPlayer = player();
+    const audioFilePath = ".response.mp3";
+    await fs.promises.writeFile(audioFilePath, Buffer.from(buffer));
+    audioPlayer.play(audioFilePath, (err: any) => {
+      if (err) console.error("Error playing audio:", err);
+    });
+  }
 
+  console.log("lastMessage:", lastMessage);
 
-    if (userMessage) {
-        await createAndRunAssistantStream({
-            assistantId: run.assistant_id,
-            userMessage: userMessage,
-            threadId: run.thread_id,
-            audio
-        });
-    }
+  let userMessage = "";
+
+  const response: { userMessage: string } = await prompts({
+    type: "text",
+    name: "userMessage",
+    message: "Enter your response:",
+  });
+  userMessage = response.userMessage;
+
+  if (userMessage) {
+    await createAndRunAssistantStream({
+      assistantId: run.assistant_id,
+      userMessage: userMessage,
+      threadId: run.thread_id,
+      audio,
+    });
+  }
 }
